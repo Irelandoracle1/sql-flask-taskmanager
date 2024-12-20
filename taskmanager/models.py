05a459c783bbeed4d436158a1d5349dfca8050f9
Irelandoracle1 <nairajnr1@gmail.com>
2024-12-20 12:50:41 +0000

    added navbar, footer and templates

---
+++ taskmanager/models.py
@@ -0,0 +1,9 @@
+from taskmanager import db
+
+
+class Category(db.Model):
+
+
+
+
+class Task(db.Model)
---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let sidenav= document.querySelectorAll('.sidenav');
+    M.sidenav.init(sidenav)
+  });
